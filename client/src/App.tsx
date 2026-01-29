2402c78159185eb2f2a9125d7b6b6af14bf8da80
Shaun Nesbitt <uksn@me.com>
2026-01-29 17:27:26 +1300

    Replace hashLocation with useHashLocation in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route, useLocation, Router as WRouter, hashLocation} from "wouter";
+import { Switch, Route, useLocation, Router as WRouter} from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -65,7 +66,7 @@ function Router() {
 
 function App() {
   return (
-    <WRouter base="/DimensionReplit" hook={hashLocation}>
+    <WRouter base="/DimensionReplit" hook={useHashLocation}>
     <QueryClientProvider client={queryClient}>
       <div className="bg-background text-foreground min-h-screen font-ui selection:bg-neon-cyan selection:text-black">
         <NotificationBanner />
